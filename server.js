0e1c1f0480cb0a7a75f329e0541e5e98cf91c83c
Soumya Jana <soumyajana@hotmail.com>
2015-07-31 14:13:27 +0530

    update server

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(3000, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:3000/');
+
+var requestListener = function (req, res) {
+  res.writeHead(200);
+  res.end('Hello, World!\n');
+}
+
+var server_port = process.env.PORT || 8080
+var server_ip_address = process.env.IP || '127.0.0.1'
+
+var server = http.createServer(requestListener); 
+
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", on port " + server_port )
+});
